79d87e7263cce8244248e52349725c84256408f0
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 16:37:29 +0900

    Update lowest common ancestor.cpp

--- Trees/lowest common ancestor.cpp
+++ Trees/lowest common ancestor.cpp
@@ -1,1 +1,26 @@
+// LCAInit(root, 1)
+int LCAInit(int now, int depth)
+{
+	dep[now] = depth;
+	for(int nxt : Gr[now])
+		if (!dep[nxt])
+		{
+			dp[nxt][0] = now;
+			LCAInit(nxt, depth + 1);
+		}
+}
 
+/*
+* N ≤ 100,000 -> j < 18
+* N ≤ 200,000 -> j < 19
+* N ≤ 500,000 -> j < 20
+* .
+* .
+* .
+*/
+void TableDP()
+{
+	for (int j(1); j < 18; j++)
+		for (int i(1); i <= n; i++)
+			dp[i][j] = dp[dp[i][j - 1]][j - 1];
+}
