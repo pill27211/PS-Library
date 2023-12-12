4cc7968659b0803a5c4bf897ce5e9a1f89fd6f69
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 17:04:14 +0900

    Update Articulation Points.cpp

--- Graphs/Articulation Points.cpp
+++ Graphs/Articulation Points.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+const int MAXN(1e5 + 3);
+
+vector <int> Gr[MAXN], in(MAXN);
+bitset <MAXN> is_art;
+
+int cur;
+int getArtPoint(int now, int is_root)
+{
+	int _min(in[now] = ++cur), sz{};
+	for (int nxt : Gr[now])
+	{
+		if (!in[nxt])
+		{
+			int cur(getArtPoint(nxt, 0));
+			if (in[now] <= cur && !is_root)
+				is_art[now] = 1;
+
+			_min = min(_min, cur);
+			sz++;
+		}
+		_min = min(_min, in[nxt]);
+	}
+	if (is_root && sz > 1)
+		is_art[now] = 1;
+
+	return _min;
+}
