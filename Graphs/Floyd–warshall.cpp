2c64a1a471b5a87827dd3acb9be84785c9bc8798
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 17:28:52 +0900

    Update Floyd–warshall.cpp

--- Graphs/Floyd–warshall.cpp
+++ Graphs/Floyd–warshall.cpp
@@ -1,1 +1,16 @@
+const int MAXN(505);
 
+int dp[MAXN][MAXN];
+
+void Floyd()
+{
+	memset(dp, 0x3f3f3f3f, sizeof dp);
+	for (int i(1); i <= n; i++)
+		dp[i][i] = 0;
+
+	for (int k(1); k <= n; k++)
+		for (int i(1); i <= n; i++)
+			for (int j(1); j <= n; j++)
+				if (dp[i][j] > dp[i][k] + dp[k][j])
+					dp[i][j] = dp[i][k] + dp[k][j];
+}
