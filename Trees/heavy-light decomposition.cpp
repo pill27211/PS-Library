9c396fefc11606fd78f4c5203a5e50a2f2cfef34
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 16:30:44 +0900

    Update heavy-light decomposition.cpp

--- Trees/heavy-light decomposition.cpp
+++ Trees/heavy-light decomposition.cpp
@@ -1,1 +1,25 @@
+void hldInit(int now)
+{
+	sz[now] = 1;
+	for (int nxt : Gr[now])
+	{
+		if (sz[nxt]) continue;
+		dep[nxt] = dep[now] + 1;
+		par[nxt] = now;
+		hldInit(nxt);
+		sz[now] += sz[nxt];
+		tree[now].push_back(nxt);
 
+		if (sz[nxt] > sz[tree[now][0]]) swap(tree[now][0], tree[now].back());
+	}
+}
+int cur;
+void hldETT(int now)
+{
+	in[now] = ++cur;
+	for (int nxt : tree[now])
+	{
+		top[nxt] = nxt ^ tree[now][0] ? nxt : top[now];
+		hldETT(nxt);
+	}
+}
