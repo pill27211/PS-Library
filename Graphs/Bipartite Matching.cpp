bcc0f4751e7bd17e32380d926f04aea82e545f9e
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 17:25:21 +0900

    Update Bipartite Matching.cpp

--- Graphs/Bipartite Matching.cpp
+++ Graphs/Bipartite Matching.cpp
@@ -3,13 +3,13 @@ const int MAXN(505);
 vector <int> Visit(MAXN), assign(MAXN);
 vector <int> Gr[MAXN];
 
-int f(int now)
+int Match(int now)
 {
 	for (int& nxt : Gr[now])
 		if (!Visit[nxt])
 		{
 			Visit[nxt] = 1;
-			if (!assign[nxt] || f(assign[nxt]))
+			if (!assign[nxt] || Match(assign[nxt]))
 				return assign[nxt] = now;
 		}
 	return 0;
