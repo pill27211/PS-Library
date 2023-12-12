af38d49dbbcc884fbcba2c1c51ab24e105ec8194
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-12 16:49:11 +0900

    Update Articulation Bridges.cpp

--- Graphs/Articulation Bridges.cpp
+++ Graphs/Articulation Bridges.cpp
@@ -1,1 +1,23 @@
+const int MAXN(1e5 + 3);
 
+vector <pair<int, int>> art_eg;
+vector <int> Gr[MAXN], in(MAXN);
+
+int cur;
+int getArtEdge(int now, int prev)
+{
+	int _min(in[now] = ++cur);
+	for (int nxt : Gr[now])
+	{
+		if (nxt == prev) continue;
+		if (!in[nxt])
+		{
+			int cur(getArtEdge(nxt, now));
+			if (in[now] < cur)
+				art_eg.emplace_back(now, nxt);
+			_min = min(_min, cur);
+		}
+		_min = min(_min, in[nxt]);
+	}
+	return _min;
+}
