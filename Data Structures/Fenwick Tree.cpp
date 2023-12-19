521a412e809095af4b42d67512d2dc64e153807f
pill27211 <120912574+pill27211@users.noreply.github.com>
2023-12-20 01:21:55 +0900

    Update Fenwick Tree.cpp

--- Data Structures/Fenwick Tree.cpp
+++ Data Structures/Fenwick Tree.cpp
@@ -1,1 +1,16 @@
+using ll = long long;
 
+const int MAXN(1e5 + 1);
+
+ll seg[MAXN];
+void update(ll i, ll val)
+{
+	while (i < MAXN)
+		seg[i] += val, i += i & -i;
+}
+ll query(ll i, ll res = 0)
+{
+	while (i)
+		res += seg[i], i -= i & -i;
+	return res;
+}
